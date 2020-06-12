88b2ab6ee4621d27aa07da2c27a7a68b909b8bee
Andreas Pirkner <andreas.pirkner@gmx.net>
2020-06-12 14:25:09 +0200

    finished

---
+++ scripts/like.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme1").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes1").text(count);
+        });
+    });
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme2").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes2").text(count);
+        });
+    });
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme3").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes3").text(count);
+        });
+    });
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme4").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes4").text(count);
+        });
+    });
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme5").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes5").text(count);
+        });
+    });
+
+$(document).ready(function()
+    {
+        var count = 0;
+        $(".clickme6").on("click", function(event)
+        {
+            event.preventDefault();
+            count++;
+            $("p#likes6").text(count);
+        });
+    });
